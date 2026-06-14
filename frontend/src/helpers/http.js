cb26538b979694c79e290d42d46813d4c01c3e1a
BATeR23 <baterhismatullin@yandex.ru>
2026-06-14 08:16:59 +0500

    add1223

--- frontend/src/helpers/http.js
+++ frontend/src/helpers/http.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE_URL = 'https://sweet-reverence-copy-production.up.railway.app'
+const API_BASE_URL = 'https://sweet-reverence-copy-production.up.railway.app/api'
 
 const http = axios.create({
     baseURL: API_BASE_URL,
