33c5413a89094fb023b713666f426ac1a4c2174b
BATeR23 <baterhismatullin@yandex.ru>
2026-06-14 08:16:14 +0500

    add1223

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -3,7 +3,9 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'register', 'logout'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'https://independent-embrace-production-a806.up.railway.app', // Домен вашего фронтенда
+    ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
