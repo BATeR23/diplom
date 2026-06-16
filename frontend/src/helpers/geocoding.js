1a9d5e7a809aa993f7ccc0c97c9dd26263dedf2f
BATeR23 <baterhismatullin@yandex.ru>
2026-06-17 00:51:08 +0500

    add1223

--- frontend/src/helpers/geocoding.js
+++ frontend/src/helpers/geocoding.js
@@ -3,12 +3,13 @@ import http from './http'
 export async function searchAddresses(query, options = {}) {
     const { limit = 5, addressdetails = true, extratags = false } = options
 
+    // ПРЕОБРАЗУЕМ булевы значения в строки '1' и '0' для бэкенда
     const response = await http.get('/geocoding/search', {
         params: {
             q: query,
             limit,
-            addressdetails,
-            extratags,
+            addressdetails: addressdetails ? '1' : '0',   // Явно передаем как строку '1' или '0'
+            extratags: extratags ? '1' : '0',            // Явно передаем как строку '1' или '0'
         },
     })
 
